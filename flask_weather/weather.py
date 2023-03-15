64a58120faa042c397f0282c64bb636aa4671cf1
Barton Elles <bartonjelles3@gmail.com>
2023-03-15 02:42:45 -0700

    Simple weather app that gives temp from ZIP.
    Backend queries OpenWeatherMap.

---
+++ flask_weather/weather.py
@@ -0,0 +1,26 @@
+'Simple Flask app that displays the current temperature at a given ZIP.'
+import flask
+import requests
+import json
+
+application = flask.Flask(__name__)
+
+
+@application.route('/')
+def home():
+    'Weather homepage.'
+    return flask.render_template('home.html')
+
+@application.route('/get_weather', methods=['GET'])
+def get_weather():
+    'Gets the weather via an API call to a backend that queries OpenWeatherMap.'
+    data = {'zip': flask.request.args.get('zip')}
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post("http://127.0.0.1:5001/weather_query",
+                              data=json.dumps(data), headers=headers)
+    return flask.render_template('home.html',
+                                  temperature=int(response.json()['temperature']))
+
+
+if __name__ == '__main__':
+    application.run(host="0.0.0.0", port=8080)
---
+++ weather_query_service/query_service.py
@@ -0,0 +1,32 @@
+'Queries OpenWeatherMap for current temperature based on ZIP'
+
+from owm_key import API_KEY
+import flask
+import requests
+
+APP = flask.Flask(__name__)
+
+@APP.route('/weather_query', methods=['POST'])
+def weather_query():
+    'Takes ZIP, queries OWM for latitude, longitude to request current temp in location.'
+    req = flask.request.get_json()
+    req_zip = req['zip']
+    try:
+        location_resp = requests.get(f'http://api.openweathermap.org/geo/1.0/zip?zip={req_zip},US&appid={API_KEY}')
+        location_resp.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        raise SystemExit(err)
+    
+    lat, lon = location_resp.json()['lat'], location_resp.json()['lon']
+
+    try:
+        weather_resp = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=imperial&appid={API_KEY}')
+        weather_resp.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        raise SystemExit(err)
+    
+    temp = weather_resp.json()['main']['temp']
+    return flask.jsonify({'temperature': temp})
+
+if __name__ == '__main__':
+    APP.run(debug=True, port=5001)
