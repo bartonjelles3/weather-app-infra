a4621d24947e1af3ded6c1676b95391016fdac57
Barton Elles <bartonjelles3@gmail.com>
2023-03-20 02:21:55 -0700

    Add simple test for flask_weather

---
+++ flask_weather/weather_test.py
@@ -0,0 +1,25 @@
+import unittest
+import weather
+
+_WEBPAGE = """<!DOCTYPE html>
+<html>
+  <head>
+    <title>Weather App</title>
+  </head>
+  <body>
+    <h1>Weather App</h1>
+    <form action="get_weather" method="get">
+      <input type="text" name="zip" placeholder="Enter ZIP code">
+      <button type="submit">Get Weather</button>
+    </form>
+    
+  </body>
+</html>"""
+
+class WeatherAppTests(unittest.TestCase):
+    def test_home_rendered(self):
+        with weather.application.app_context():
+          self.assertEqual(weather.home(), _WEBPAGE)
+
+if __name__ == '__main__':
+    unittest.main()
