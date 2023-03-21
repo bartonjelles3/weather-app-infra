0cb5cd7782a5e043dde1066f2dbeaf37354ef41a
Barton Elles <bartonjelles3@gmail.com>
2023-03-20 23:20:07 -0700

    Test trigger 12

--- flask_weather/test_weather.py
+++ flask_weather/test_weather.py
@@ -18,7 +18,7 @@ _WEBPAGE = """<!DOCTYPE html>
 
 class WeatherAppTests(unittest.TestCase):
     def test_home_rendered(self):
-        'test11'
+        'test12'
         with weather.application.app_context():
           self.assertNotEqual(weather.home(), _WEBPAGE)
 
