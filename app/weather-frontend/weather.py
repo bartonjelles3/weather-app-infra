5811eee5690781036bd05fbe5e36d3b154c59234
Barton Elles <bartonjelles3@gmail.com>
2023-05-29 23:53:47 -0700

    Add error handling and beautify

--- app/weather-frontend/weather.py
+++ app/weather-frontend/weather.py
@@ -1,4 +1,3 @@
-'Simple Flask app that displays the current temperature at a given ZIP.'
 import flask
 import requests
 import json
@@ -24,10 +23,19 @@ def get_weather():
     data = {'zip': flask.request.args.get('zip')}
     headers = {'Content-Type': 'application/json'}
     logging.info(f'Sending request to http://weather-query-svc.default.svc.cluster.local:8081/weather_query')
-    response = requests.post("http://weather-query-svc.default.svc.cluster.local:8081/weather_query",
-                              data=json.dumps(data), headers=headers)
-    return flask.render_template('home.html',
-                                  temperature=int(response.json()['temperature']))
+    
+    try:
+        response = requests.post("http://weather-query-svc.default.svc.cluster.local:8081/weather_query",
+                                 data=json.dumps(data), headers=headers)
+        response.raise_for_status() 
+        
+        return flask.render_template('home.html', temperature=int(response.json()['temperature']))
+    
+    except requests.exceptions.RequestException as e:
+        logging.error(f'Error connecting to the backend: {str(e)}')
+        error_message = 'Error: Query service is currently unavailable.'
+        
+        return flask.render_template('home.html', error_message=error_message), 500
 
 @application.route('/metrics')
 def metrics():
